9d954945d635986dcf15016bb33ca662c132bcae
lava <lava@midgard.by>
2012-12-02 15:29:15 +0300

    Duplicated card output on bold

--- shell/show_mono.sh
+++ shell/show_mono.sh
@@ -8,6 +8,17 @@ alias b-='tput sgr0'
 
 sample() {
     echo -e "Showing monospace font: $(b)${FONT_NAME}$(b-)"
+    card | duplicate_in_bold
+}
+
+duplicate_in_bold() {
+    while read line
+    do
+        echo -e "${line}$(b)${line}$(b-)"
+    done
+}
+
+card() {
     cat <<EOS
 ┌←─ ~ ────────────────────────────○[↓]→┐
 │                                      │
