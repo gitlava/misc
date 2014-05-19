bfdd107363a94b5c1f9239b43f5c786e604b0008
Eugene Lavnikevich <e.lavnikevich@sam-solutions.com>
2014-05-19 10:48:32 +0300

    Refactored statistics into separate class

--- python/championship.py
+++ python/championship.py
@@ -202,88 +202,142 @@ class Championship(object):
     return group_table
 
   def team_statistics(self, team):
-    statistics = {'name': team, 'games_played': 0, 'goals_for': 0, 'goals_against': 0, 'points': 0}
+    statistics = TeamStatistics(team)
     for key in self.games:
       game = self.games[key]
       if (game.team_a == team):
-        statistics['games_played'] += 1
-        statistics['goals_for'] += game.score['a']
-        statistics['goals_against'] += game.score['b']
-        statistics['points'] += game.team_a_points()
+        statistics.add_game_statistics({'goals_for':game.score['a'], 'goals_against':game.score['b'], 'points':game.team_a_points()})
       elif (game.team_b == team):
-        statistics['games_played'] += 1
-        statistics['goals_for'] += game.score['b']
-        statistics['goals_against'] += game.score['a']
-        statistics['points'] += game.team_b_points()
+        statistics.add_game_statistics({'goals_for':game.score['b'], 'goals_against':game.score['a'], 'points':game.team_b_points()})
     return statistics
 
   def print_group(self, group_name):
-    statistics = sorted(self.group_statistics(group_name))
+    statistics = sorted(self.group_statistics(group_name), reverse = True)
     for team in statistics:
       print team
 
+class TeamStatistics(object):
+  def __init__(self, team):
+    self.team          = team
+    self.games_played  = 0
+    self.goals_for     = 0
+    self.goals_against = 0
+    self.points        = 0
+
+  def __str__(self):
+    return str({'team':self.team, 'games_played':self.games_played, 'goals_for':self.goals_for, 'goals_against':self.goals_against, 'points':self.points})
+
+  @property
+  def team(self):
+    return self._team
+
+  @team.setter
+  def team(self, value):
+    self._team = value
+
+  @property
+  def games_played(self):
+    return self._games_played
+
+  @games_played.setter
+  def games_played(self, value):
+    self._games_played = value
+
+  @property
+  def goals_against(self):
+    return self._goals_against
+
+  @goals_against.setter
+  def goals_against(self, value):
+    self._goals_against = value
+
+  @property
+  def goals_for(self):
+    return self._goals_for
+
+  @goals_for.setter
+  def goals_for(self, value):
+    self._goals_for = value
+
+  @property
+  def points(self):
+    return self._points
+
+  @points.setter
+  def points(self, value):
+    self._points = value
+
+  def add_game_statistics(self, params):
+    self.games_played  += 1
+    self.goals_for     += params['goals_for']
+    self.goals_against += params['goals_against']
+    self.points        += params['points']
+
 
 championship2014 = Championship()
+
 championship2014.init_group('A', ['Canada', 'Czech', 'Denmark', 'France', 'Italy', 'Norway', 'Slovakia', 'Sweden'])
 championship2014.init_group('B', ['Belarus', 'Finland', 'Germany', 'Kazakhstan', 'Latvia', 'Russia', 'Switzerland', 'USA'])
 
 #9 May
-championship2014.add_game_info(Game('France',       'Canada',  [3,2,False]))
-championship2014.add_game_info(Game('Switzerland',  'Russia',  [0,5,True]))
-championship2014.add_game_info(Game('Slovakia',     'Czech',   [2,3,False]))
-championship2014.add_game_info(Game('Belarus',      'USA',     [1,6,True]))
+championship2014.add_game_info(Game('France',      'Canada',      [3,2,False]))
+championship2014.add_game_info(Game('Switzerland', 'Russia',      [0,5,True]))
+championship2014.add_game_info(Game('Slovakia',    'Czech',       [2,3,False]))
+championship2014.add_game_info(Game('Belarus',     'USA',         [1,6,True]))
 #10 May
-championship2014.add_game_info(Game('Italy',       'Norway',       [0,3,True]))
-championship2014.add_game_info(Game('Kazakhstan',  'Germany',      [1,2,False]))
-championship2014.add_game_info(Game('Sweden',      'Denmark',      [3,0,True]))
-championship2014.add_game_info(Game('Finland',     'Latvia',       [2,3,True]))
-championship2014.add_game_info(Game('Canada',      'Slovakia',     [4,1,True]))
-championship2014.add_game_info(Game('USA',         'Switzerland',  [3,2,True]))
+championship2014.add_game_info(Game('Italy',       'Norway',      [0,3,True]))
+championship2014.add_game_info(Game('Kazakhstan',  'Germany',     [1,2,False]))
+championship2014.add_game_info(Game('Sweden',      'Denmark',     [3,0,True]))
+championship2014.add_game_info(Game('Finland',     'Latvia',      [2,3,True]))
+championship2014.add_game_info(Game('Canada',      'Slovakia',    [4,1,True]))
+championship2014.add_game_info(Game('USA',         'Switzerland', [3,2,True]))
 #11 May
-championship2014.add_game_info(Game('France',   'Italy',       [1,2,True]))
-championship2014.add_game_info(Game('Germany',  'Latvia',      [3,2,True]))
-championship2014.add_game_info(Game('Norway',   'Denmark',     [4,3,True]))
-championship2014.add_game_info(Game('Belarus',  'Kazakhstan',  [4,1,True]))
-championship2014.add_game_info(Game('Sweden',   'Czech',       [4,3,False]))
-championship2014.add_game_info(Game('Finland',  'Russia',      [2,4,True]))
+championship2014.add_game_info(Game('France',      'Italy',       [1,2,True]))
+championship2014.add_game_info(Game('Germany',     'Latvia',      [3,2,True]))
+championship2014.add_game_info(Game('Norway',      'Denmark',     [4,3,True]))
+championship2014.add_game_info(Game('Belarus',     'Kazakhstan',  [4,1,True]))
+championship2014.add_game_info(Game('Sweden',      'Czech',       [4,3,False]))
+championship2014.add_game_info(Game('Finland',     'Russia',      [2,4,True]))
 #12 May
-championship2014.add_game_info(Game('Slovakia',     'France',   [3,5,True]))
-championship2014.add_game_info(Game('Switzerland',  'Belarus',  [3,4,True]))
-championship2014.add_game_info(Game('Czech',        'Canada',   [3,4,True]))
-championship2014.add_game_info(Game('Russia',       'USA',      [6,1,True]))
+championship2014.add_game_info(Game('Slovakia',    'France',      [3,5,True]))
+championship2014.add_game_info(Game('Switzerland', 'Belarus',     [3,4,True]))
+championship2014.add_game_info(Game('Czech',       'Canada',      [3,4,True]))
+championship2014.add_game_info(Game('Russia',      'USA',         [6,1,True]))
 #13 May
-championship2014.add_game_info(Game('Italy',       'Denmark',  [1,4,True]))
-championship2014.add_game_info(Game('Germany',     'Finland',  [0,4,True]))
-championship2014.add_game_info(Game('Norway',      'Sweden',   [1,2,True]))
-championship2014.add_game_info(Game('Kazakhstan',  'Latvia',   [4,5,True]))
+championship2014.add_game_info(Game('Italy',       'Denmark',     [1,4,True]))
+championship2014.add_game_info(Game('Germany',     'Finland',     [0,4,True]))
+championship2014.add_game_info(Game('Norway',      'Sweden',      [1,2,True]))
+championship2014.add_game_info(Game('Kazakhstan',  'Latvia',      [4,5,True]))
 #14 May
-championship2014.add_game_info(Game('Czech',        'Italy',       [2,0,True]))
-championship2014.add_game_info(Game('Switzerland',  'Germany',     [3,2,True]))
-championship2014.add_game_info(Game('Slovakia',     'Norway',      [5,2,True]))
-championship2014.add_game_info(Game('Russia',       'Kazakhstan',  [7,2,True]))
+championship2014.add_game_info(Game('Czech',       'Italy',       [2,0,True]))
+championship2014.add_game_info(Game('Switzerland', 'Germany',     [3,2,True]))
+championship2014.add_game_info(Game('Slovakia',    'Norway',      [5,2,True]))
+championship2014.add_game_info(Game('Russia',      'Kazakhstan',  [7,2,True]))
 #15 May
-championship2014.add_game_info(Game('Canada',   'Denmark',  [6,1,True]))
-championship2014.add_game_info(Game('USA',      'Latvia',   [5,6,True]))
-championship2014.add_game_info(Game('Sweden',   'France',   [2,1,True]))
-championship2014.add_game_info(Game('Finland',  'Belarus',  [2,0,True]))
+championship2014.add_game_info(Game('Canada',      'Denmark',     [6,1,True]))
+championship2014.add_game_info(Game('USA',         'Latvia',      [5,6,True]))
+championship2014.add_game_info(Game('Sweden',      'France',      [2,1,True]))
+championship2014.add_game_info(Game('Finland',     'Belarus',     [2,0,True]))
 #16 May
-championship2014.add_game_info(Game('Canada',   'Italy',        [6,1,True]))
-championship2014.add_game_info(Game('USA',      'Kazakhstan',   [4,3,False]))
-championship2014.add_game_info(Game('Sweden',   'Slovakia',     [3,1,True]))
-championship2014.add_game_info(Game('Finland',  'Switzerland',  [3,2,False]))
+championship2014.add_game_info(Game('Canada',      'Italy',       [6,1,True]))
+championship2014.add_game_info(Game('USA',         'Kazakhstan',  [4,3,False]))
+championship2014.add_game_info(Game('Sweden',      'Slovakia',    [3,1,True]))
+championship2014.add_game_info(Game('Finland',     'Switzerland', [3,2,False]))
 #17 May
-championship2014.add_game_info(Game('France',       'Norway',      [5,4,False]))
-championship2014.add_game_info(Game('Latvia',       'Russia',      [1,4,True]))
-championship2014.add_game_info(Game('Denmark',      'Czech',       [4,3,False]))
-championship2014.add_game_info(Game('Belarus',      'Germany',     [5,2,True]))
-championship2014.add_game_info(Game('Slovakia',     'Italy',       [4,1,True]))
-championship2014.add_game_info(Game('Switzerland',  'Kazakhstan',  [6,2,True]))
+championship2014.add_game_info(Game('France',      'Norway',      [5,4,False]))
+championship2014.add_game_info(Game('Latvia',      'Russia',      [1,4,True]))
+championship2014.add_game_info(Game('Denmark',     'Czech',       [4,3,False]))
+championship2014.add_game_info(Game('Belarus',     'Germany',     [5,2,True]))
+championship2014.add_game_info(Game('Slovakia',    'Italy',       [4,1,True]))
+championship2014.add_game_info(Game('Switzerland', 'Kazakhstan',  [6,2,True]))
 #18 May
-championship2014.add_game_info(Game('Canada',  'Sweden',  [3,2,False]))
-championship2014.add_game_info(Game('USA',  'Finland',  [3,1,True]))
+championship2014.add_game_info(Game('Canada',      'Sweden',      [3,2,False]))
+championship2014.add_game_info(Game('USA',         'Finland',     [3,1,True]))
 
 
 print championship2014
 
+championship2014.print_group('A')
+print
 championship2014.print_group('B')
 
